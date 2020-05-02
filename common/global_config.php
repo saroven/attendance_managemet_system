22ee1be12594485126173ca6535464c139db2ae8
saroven <ashah3562@gmail.com>
2020-05-03 03:13:24 +0600

    fixed error url

--- common/global_config.php
+++ common/global_config.php
@@ -4,13 +4,13 @@
 
 	$db_host = "localhost";
 	$db_user = "root";
-	$db_password = "shahalam1";
+	$db_password = "";
 	$db_name = "attendance_db";
 
 	$photo_upload_dir = "uploads/users/photos/";
 
 	function goToError($url){
-		// $url = $base_url."/common/403.php";
+		$url = $base_url."/common/403.php";
     	echo "<script> location.href='".$url."'; </script>";
 	}
 
